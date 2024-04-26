175c9f931deeb68c72ccbdb3b9a37b1602180292
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2024-04-27 06:19:40 +0800

    [feature] add new feature add line notify support add multiple instance support
    Signed-off-by: bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -3,47 +3,87 @@ import os
 import json
 from dotenv import load_dotenv
 from discord_webhook import DiscordWebhook, DiscordEmbed
-
+import requests
 from time import sleep
-
+import concurrent.futures
+# import asyncio
 load_dotenv()
 
-Webhook_URL = os.getenv("WEBHOOK_URL")
+countdown = True
+Webhook_URL = os.getenv("WEBHOOK_URL").split(",")
 city = os.getenv("CITY")
 Area = os.getenv("AREA")
+line_token = os.getenv("ACCESS_token").split(",")
+
 
 with open('file.json', mode='r', encoding='UTF8') as jfile:
     jdate1 = json.load(jfile)
     
-ggg = re.sub(r"(\d)$", "\\1級", jdate1["1"]).replace("-", "弱").replace("+", "強")
+intensity = re.sub(r"(\d)$", "\\1級", jdate1["1"]).replace("-", "弱").replace("+", "強")
 sec = int(jdate1["2"]) + 2
 
-webhook = DiscordWebhook(url=Webhook_URL, username="地牛Wake UP!", 
-        avatar_url="https://cdn.discordapp.com/attachments/825307887219114034/902494942352519168/FB_IMG_1635241955969.jpg",
-        content=f'倒數{sec}秒抵達!')
+def lineNotifyMessage(line_token, sec, city, Area, intensity):
+    linecontent = f"\n倒數{sec}秒後抵達!"+'\n'+f"{city} {Area} {intensity}"
+    headers = {
+        'Authorization': f'Bearer {line_token}',
+    }
+    requests.post('https://notify-api.line.me/api/notify', headers=headers, data={'message': linecontent})
+    if int(intensity[0]) >= 4:
+        requests.post('https://notify-api.line.me/api/notify', headers=headers, data={'message': '\n震度較大 注意安全'})
 
-embed = DiscordEmbed(title=':rotating_light:【地震速報】', description='慎防搖晃(預估震度)', color='03b2f8')
-embed.set_author(name='Taiwan EEW System', 
-        icon_url='https://media.discordapp.net/attachments/345147297539162115/732527807435112478/EEW.png')
-embed.set_timestamp()
-embed.add_embed_field(name=f"{city}", value=f"{Area} {ggg}")
-webhook.add_embed(embed)
 
-sent_webhook = webhook.execute()
+def discordNotifyMessage(Webhook_URL, sec, city, Area, intensity,countdown):
+    webhook = DiscordWebhook(url=Webhook_URL, username="地牛Wake UP!", 
+            avatar_url="https://cdn.discordapp.com/attachments/825307887219114034/902494942352519168/FB_IMG_1635241955969.jpg",
+            content=f'@everyone \n倒數{sec}秒抵達!')
+    if int(intensity[0]) >= 4:
+        embed = DiscordEmbed(title=':rotating_light:【地震速報】', description='慎防搖晃(預估震度)', color='ff0000')
+    else:
+        embed = DiscordEmbed(title=':rotating_light:【地震速報】', description='慎防搖晃(預估震度)', color='4DFD4D')
+    embed.set_author(name='Powered by 地牛Wake UP!', 
+            icon_url='https://cdn.discordapp.com/attachments/825307887219114034/902494942352519168/FB_IMG_1635241955969.jpg')
+    embed.set_timestamp()
+    embed.add_embed_field(name=f"{city}", value=f"{Area} {intensity}")
+    webhook.add_embed(embed)
+    webhook.execute()
+    # if countdown == False:
+    #     webhook.content=f'@everyone \n倒數{sec}秒抵達!\n***倒數已停止***'
+    #     embed.set_color('ff0000')
+    #     webhook.edit()
+    #     return
+    if sec == 0:
+        webhook.content = f'@everyone \n已抵達!'
+        embed.set_color('03b2f8')
+        webhook.edit()
+        return
+    
+    while (sec and countdown):
+        sec = sec - 1
 
-sec1 = int(sec)
-aaa = sent_webhook
-while sec1:
-    sec1 = sec1 - 1
-
-    sleep(0.6)
-
-    webhook.content = f'倒數{sec1}秒後到達!'
-    sent_webhook = webhook.edit(aaa)
-
-    if sec1 == 0:
         sleep(0.6)
-        webhook.content = f'已抵達!'
-        sent_webhook = webhook.edit(aaa)
-        break
- 
+
+        webhook.content = f'@everyone \n倒數{sec}秒後到達!'
+        webhook.edit()
+
+        if sec == 0:
+            sleep(0.6)
+            webhook.content = f'@everyone \n已抵達!'
+            embed.set_color('03b2f8')
+            webhook.edit()
+            break
+    
+
+def main():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        [executor.submit(lineNotifyMessage(x, sec, city, Area, intensity)) for x in line_token]
+        pass
+
+    # if len(Webhook_URL) !=1:
+        # countdown = False
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        [executor.submit(discordNotifyMessage, y, sec, city, Area, intensity, countdown) for y in Webhook_URL]
+        pass
+    
+
+main()
