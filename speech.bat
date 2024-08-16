fb96678dce3c8b3397303e592ef808e6c1e3023b
Bradly Chang <bradly093@gmail.com>
2024-08-16 08:56:28 +0800

    [feature] add alert logs (#8)
    Signed-off-by: bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>

--- speech.bat
+++ speech.bat
@@ -16,4 +16,7 @@
 @echo {"1":"%1","2":"%2"} >file.json
 
 
-PowerShell -WindowStyle Hidden -Command "python bot.py" 
+@REM PowerShell -WindowStyle Hidden -Command "python bot.py" 
+set currentTime=%date% %time%
+echo [%currentTime%] With intensity of %1 in %2 seconds. >>logs.txt
+
